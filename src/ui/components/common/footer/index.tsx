4031e4fdcd88dc8d8479cf047260295385bcfbf4
Oleh <my.mail.olech@gmail.com>
2023-01-10 21:35:33 +0200

    footer

--- src/ui/components/common/footer/index.tsx
+++ src/ui/components/common/footer/index.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
+import SubscribeForm from "../subscribe-form";
 import styles from "./styles.module.scss";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles["content-wrapper"]}>
-        <section>
+        <section className={styles["nft-marketplace-info"]}>
           <h5>NFT Marketplace</h5>
           <p>NFT marketplace UI created with Anima for Figma.</p>
-          <p>Join our community</p>
+          <p className={styles["join-community"]}>Join our community</p>
           <ul>
             <li>
               <a href="">
@@ -27,8 +28,19 @@ const Footer = () => {
             </li>
           </ul>
         </section>
-        <section></section>
-        <section></section>
+        <section className={styles.explore}>
+          <h5>Explore</h5>
+          <div className={styles.navigation}>
+            <Link href="/marketplace">Marketplace</Link>
+            <Link href="/rankings">Rankings</Link>
+            <Link href="/connect-wallet">Connect a wallet</Link>
+          </div>
+        </section>
+        <section className={styles.subscribe}>
+          <h5>Join our weekly digest</h5>
+          <p>Get exclusive promotions & updates straight to your inbox.</p>
+          <SubscribeForm />
+        </section>
       </div>
       <section></section>
     </footer>
